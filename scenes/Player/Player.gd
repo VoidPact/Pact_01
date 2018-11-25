7df06e22a28eedf723512739842c62602ed29ef7
dims <emilisenbecker@yahoo.dk>
2018-11-25 16:31:25 +0100

    Add player and networking

---
+++ scenes/Player/Player.gd
@@ -0,0 +1,13 @@
+extends KinematicBody2D
+
+func _physics_process(delta):
+	handle_movement()
+
+func handle_movement():
+	if Input.is_action_pressed("move_left"):
+		rpc("move", -10)
+	if Input.is_action_pressed("move_right"):
+		rpc("move", 10)
+
+sync func move(dist):
+	position.x += dist
