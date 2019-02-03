df92e1eb64f75cedaf04c73c72a1ae447d44b9f2
jc <jenscarlwow@gmail.com>
2019-02-03 14:11:31 +0100

    Added basic movement, friction and gravity to player

--- scenes/Player/Player.gd
+++ scenes/Player/Player.gd
@@ -1,5 +1,20 @@
 extends KinematicBody2D
 
+
+# MOVEMENT 
+const XINIT = 500
+const YINIT = 200
+
+const ACCEL = 100
+const MAX_X_SPEED = 500
+
+const GRAV = 50
+const FRIC = 0.2
+
+var velocity = Vector2()
+
+
+# META INFO
 var id
 var info
 
@@ -7,14 +22,31 @@ func init(id, info):
 	set_name(str(id))
 	id = id
 	info = info
+	position.x = XINIT
+	position.y = YINIT
+	
+	
 
 # This is really bad
-# RPCs should probably be by themselves and only call necessary stuff (e.g. set position)
 func handle_movement():
 	if Input.is_action_pressed("move_left"):
-		rpc("move", position.x - 10)
+		velocity.x -= ACCEL
 	if Input.is_action_pressed("move_right"):
-		rpc("move", position.x + 10)
-
-sync func move(dist):
-	position.x = dist
+		velocity.x += ACCEL
+	
+	velocity.y += GRAV
+	
+	if Input.is_action_pressed("move_left") == Input.is_action_pressed("move_right"):
+		velocity.x = lerp(velocity.x, 0, FRIC)
+	
+	velocity.x = clamp(velocity.x, -MAX_X_SPEED, MAX_X_SPEED)
+	#if not (Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right")):
+	#	if velocity.x > 0:
+	#		velocity.x -= FRIC
+	#	elif velocity.x < 0:
+	#		velocity.x += FRIC 
+		
+	
+	move_and_slide(velocity)
+	
+	
