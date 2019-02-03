de801c90ddcb7a0d6e0ba1804bf4ac63ccfcdd28
jc <jenscarlwow@gmail.com>
2019-02-03 15:00:11 +0100

    Added Jump

--- scenes/Player/Player.gd
+++ scenes/Player/Player.gd
@@ -5,11 +5,15 @@ extends KinematicBody2D
 const XINIT = 500
 const YINIT = 200
 
-export var ACCEL = 200
+export var X_ACCEL = 200
+export var Y_ACCEL = 1000
+
 const MAX_X_SPEED = 500
+const MAX_Y_SPEED = 1000
 
 const GRAV = 50
 const FRIC = 0.2
+const FLOOR_NORMAL = Vector2(0,-1)
 
 var velocity = Vector2()
 
@@ -30,23 +34,22 @@ func init(id, info):
 # This is really bad
 func handle_movement():
 	if Input.is_action_pressed("move_left"):
-		velocity.x -= ACCEL
+		velocity.x -= X_ACCEL
 	if Input.is_action_pressed("move_right"):
-		velocity.x += ACCEL
+		velocity.x += X_ACCEL
+	if Input.is_action_pressed("jump") && is_on_floor():
+		velocity.y -= Y_ACCEL
 	
 	velocity.y += GRAV
 	
 	if Input.is_action_pressed("move_left") == Input.is_action_pressed("move_right"):
 		velocity.x = lerp(velocity.x, 0, FRIC)
 	
+	# CLAMP
 	velocity.x = clamp(velocity.x, -MAX_X_SPEED, MAX_X_SPEED)
-	#if not (Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right")):
-	#	if velocity.x > 0:
-	#		velocity.x -= FRIC
-	#	elif velocity.x < 0:
-	#		velocity.x += FRIC 
+	velocity.y = clamp(velocity.y, -MAX_Y_SPEED, MAX_Y_SPEED)
 		
 	
-	move_and_slide(velocity)
+	move_and_slide(velocity, FLOOR_NORMAL)
 	
 	
